c1ea62b98092c8642342a597ed23b1425fb08b24
haboubacart <haboubacart@gmail.com>
2024-06-19 23:33:58 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# know_me_by_my_AI
+## Description du Projet
+Boubé AI est un assistant virtuel conçu pour répondre aux questions des recruteurs sur le profil d'une personne. L'objectif est qu'il fournisse des réponses détaillées, convaincantes et personnalisées en mettant en valeur les éléments clés de son parcours professionnel, ses compétences et ses réalisations. 
+Le projet Flask pour le backend, GPT-4o pour le traitement du langage naturel avancé, et FAISS pour la recherche et l'indexation efficace des informations.
+
+
+## Technologies Utilisées
+- **Flask :** Framework web utilisé pour développer l'application backend de Boubé AI.
+- **GPT-4o :** Modèle de génération de texte avancé utilisé pour comprendre et générer des réponses basées sur le contexte des questions posées.
+- **Sentence-camembert-large :** Modèle pour l'embedding 
+- **FAISS :** Bibliothèque utilisée pour l'indexation rapide et la recherche de données, assurant des réponses efficaces et rapides.
+
+## Structure du Code
+
+Le projet est structuré de la manière suivante :
+
+- **app.py :** Fichier principal contenant la logique de l'application Flask.
+- **chatbot/templates/** : Répertoire contenant les templates HTML utilisés pour la présentation des réponses.
+- **chatbot/static/** : Répertoire contenant les fichiers statiques tels que les images ou les fichiers CSS.
+- **chatbot/src/chatbot.py :** Fichier contenant la logique de l'assistant virtuel utilisant GPT-4o.
+- **chatbot/src/token_manager.py :** Fichier contenant la logique de génération de connexion à l'interface de chat.
+- **train_vectorestore.ipynb :** Notebook pour entrainer le vectorestore du retriever FAISS
+
+## Comment Utiliser le Projet
+Pour lancer l'app (avec docker):
+1. Installez les dépendances listées dans ` chatbot/requirements.txt` en lançant : pip install -r  chatbot/requirements.txt depuis le dossier
+2. Entrainer le vectorestore : assurer vous d'avoir un fichier base de compétence (base_cometence.txt ou base_competence.md), votre fichier CV detaillé ou portfolio en format texte ou markdown
+3. lancer la commande : docker-composer up afin de générer puis exécuter l'image de l'application qui démarre sur la page de connexion
+4. Accéder à l'adresse (127.0.0.1:8080/generate_token) afin de générer le token de connexion 
+5. Renseinger le token génér& sur la page d'authentification (27.0.0.1:8080/). La session (token) expire au bout de 10 mins après authentification
--- chatbot/src/chatbot.py
+++ chatbot/src/chatbot.py
@@ -3,11 +3,11 @@ from langchain.schema import HumanMessage
 def custom_prompt(retriever, query):
     results = retriever.similarity_search(query, k=4)
     source_knowledge = "\n".join([x.page_content for x in results])
-    augment_prompt = f"""Tu t'appelles Boubé AI, un assistant virtuel conçu pour répondre aux questions des recruteurs sur Haboubacar Tidjani Boukari. Ta mission est de fournir des réponses détaillées, convaincantes et personnalisées en mettant en valeur les éléments clés de son parcours professionnel, ses compétences et ses réalisations. Utilise des balises HTML comme <p> pour les paragraphes, <b> pour le texte en gras, <i> pour le texte en italique, et <ul> pour les listes. Évite les titres h1 ou h2 et les sauts de ligne. Si la question ne demande pas de détails, sois synthétique. Si la question est très personnelle ou si tu ne trouves pas de réponse dans le contexte, indique que tu ne sais pas.\n
+    augment_prompt = f"""Tu t'appelles Boubé AI, un assistant virtuel conçu pour répondre aux questions des recruteurs sur [Prenom + Nom]. Ta mission est de fournir des réponses détaillées, convaincantes et personnalisées en mettant en valeur les éléments clés de son parcours professionnel, ses compétences et ses réalisations. Utilise des balises HTML comme <p> pour les paragraphes, <b> pour le texte en gras, <i> pour le texte en italique, et <ul> pour les listes. Évite les titres h1 ou h2 et les sauts de ligne. Si la question ne demande pas de détails, sois synthétique. Si la question est très personnelle ou si tu ne trouves pas de réponse dans le contexte, indique que tu ne sais pas.\n
 Directives spécifiques :\n
 1. Reste synthétique si la question ne demande pas de détails : Fournis des réponses courtes et précises pour les questions simples.\n
 2. Donne des réponses détaillées lorsque nécessaire : Pour les questions complexes, détaille le contexte, les défis, les solutions et les résultats.\n
-3. Personnalise chaque réponse : Adapte les réponses pour refléter les compétences et expériences spécifiques de Haboubacar Tidjani Boukari.\n
+3. Personnalise chaque réponse : Adapte les réponses pour refléter les compétences et expériences spécifiques de [Prenom + Nom].\n
 4. Mets en valeur les éléments clés avec des balises HTML : Utilise <b> pour les compétences clés et <i> pour les réalisations notables.\n
 5. Assure-toi de la cohérence et de la clarté : Chaque réponse doit être claire et bien structurée.
 Voici le contexte dont tu dois te servir : 
